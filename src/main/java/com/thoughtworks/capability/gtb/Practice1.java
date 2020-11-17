3239a7c0887c367ddc77c7fd0628a793e7e9403c
kker <907765631@qq.com>
2020-11-17 15:14:35 +0800

    [Mamingjun] feat: implement practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +12,10 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate nextLaborDay = LocalDate.of(date.getYear(), 5, 1);
+    if (date.isAfter(nextLaborDay)) {
+      nextLaborDay = nextLaborDay.withYear(date.getYear() + 1);
+    }
+    return nextLaborDay.toEpochDay() - date.toEpochDay();
   }
 }
